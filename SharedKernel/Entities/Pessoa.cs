954f33f065a9913b9093294a8fc87d84c8d39f12
Leonardo Vieira <leonardo.vieira0982@gmail.com>
2025-10-05 02:02:11 -0300

    GerenciaUsuario estrutura de container e banco de dados

--- SharedKernel/Entities/Pessoa.cs
+++ SharedKernel/Entities/Pessoa.cs
@@ -8,13 +8,9 @@ public abstract class Pessoa
 
     public required string Nome { get; set; }
 
-    public required string Cpf { get; set; }
+    public required string Cpf { get; set; }    
 
-    public required string Email { get; set; }
-
-    public required string Telefone { get; set; }
-
-    public required TipoUsuario TipoUsuario { get; set; }
+    public required string Telefone { get; set; }    
     
     public Endereco? Endereco { get; set; }
 }
